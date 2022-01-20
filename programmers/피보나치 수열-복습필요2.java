bc51aa11e8a5679b8e2f46c4fdd93bcc757e11e5
YJunsu03 <73802392+YJunsoo@users.noreply.github.com>
2022-01-20 23:35:28 +0900

    commit

---
+++ programmers/피보나치 수열-복습필요2.java
@@ -0,0 +1,18 @@
+class Solution {
+    
+        public int solution(int n) {
+            int arr[] = new int[n+1];
+          
+            for(int i=0; i<=n; i++) {
+                if(i==0) arr[i] = 0;
+                else if(i==1) arr[i] =1;
+                else {
+                    int sum = arr[i-2] + arr[i-1];
+                    arr[i] = sum % 1234567;
+                }
+            }
+            
+            return arr[n];
+    }
+        
+}
